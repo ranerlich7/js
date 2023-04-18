b3dd067ff4fe3a79c4569560eb40d5f6b752d423
ranerlich7 <ranerlich@gmail.com>
2023-04-18 21:30:06 +0300

    added arrow function, map, arrays

---
+++ map.js
@@ -0,0 +1,20 @@
+
+function triple(x) {
+    console.log(x * 3)
+}
+
+// definition for arrow function with name.
+const triple2 = x => console.log(x * 5)
+
+const numbers = [1, 5, 10]
+
+result = numbers.forEach(x => console.log(x * 10))
+// console.log(`hello ${result}`);
+
+result2 = numbers.map(x => x * 10)
+console.log(`result2= ${result2}`);
+
+
+
+
+
